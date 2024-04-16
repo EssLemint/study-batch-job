9c2763065646099c412956ba17741a6c21be9e26
EssLemint <limemint1396@gmail.com>
2024-04-17 00:10:33 +0900

    batch-job 2차 commit 4장 [2024-04-15]

--- src/main/java/com/batch_job/BatchJobApplication.java
+++ src/main/java/com/batch_job/BatchJobApplication.java
@@ -2,19 +2,42 @@ package com.batch_job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 public class BatchJobApplication {
+
+	@Bean
+	public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("step1", jobRepository)
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						//비즈니스 로직 작성
+						System.out.println("Hello World");
+						return RepeatStatus.FINISHED;
+					}
+				}, transactionManager)
+				.build();
+	}
+
+	@Bean
+	public Job job (JobRepository jobRepository, Step step) {
+		return new JobBuilder("job", jobRepository)
+				.start(step)
+				.build();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchJobApplication.class, args);
